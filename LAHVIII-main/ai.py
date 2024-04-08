d18424a077246013156cbeeb60d818cb094f6bc9
1smh <166179321+1smh@users.noreply.github.com>
2024-04-07 20:29:14 -0700

    Update ai.py

--- LAHVIII-main/ai.py
+++ LAHVIII-main/ai.py
@@ -4,7 +4,7 @@ def clear_text_file(filename):
     with open(filename, "w"):
         pass 
 
-genai.configure(api_key="AIzaSyC89R5-zp8_8SmjpzJP1sV_ojJsjKVJsj4")
+genai.configure(api_key="[place_holder]")
 model = genai.GenerativeModel('gemini-pro')
 response = model.generate_content("You must generate a SINGLE black hole using the format: 'Type: [type], Name: [RANDOM 8 CHARACTER STRING OF NUMBERS AND LETTERS]' followed by a newline, Description: (3 words or less) NEWLINE, Age: [AGE]")
 with open(r"C:\Users\decla\github\LAHVIII\entries.txt", "w") as w:
